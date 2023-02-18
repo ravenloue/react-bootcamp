87515a7c3ed9ea2273b0a9c32f60af466c57362a
ravenloue <42080046+ravenloue@users.noreply.github.com>
2023-02-18 12:06:40 -0600

    course correction
    was using new video but old information
    
    but i did learn to how to write comments in jsx

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import './App.css';
+import './styles.css';
 import Header from './Components/header';
-import Course from './Components/Course';
-
-const mostPopular = "JavaScript";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <h2>Courses offered by Skillsoft</h2>
-      <p></p>
-      <Course popCourse={mostPopular}/>
+
+      
     </div>
   );
 }
--- src/Components/header.js
+++ src/Components/header.js
@@ -1,9 +1,13 @@
 import React from "react";
+import chart from '../chart.gif';
 
 function Header(){
 
     return(
-        <header>This is a header</header>
+        <header>
+            <img src={ chart } id="logo" />
+			<h1><a href="index.html">Skillsoft Weight Tracker</a></h1>
+        </header>
     )
 
 }
