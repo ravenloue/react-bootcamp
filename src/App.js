4e5b95a453ed4550cd1c6d4e1060242b816a735f
ravenloue <42080046+ravenloue@users.noreply.github.com>
2023-02-18 11:42:08 -0600

    module 2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Header from './Components/header';
+import Course from './Components/Course';
+
+const mostPopular = "JavaScript";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
+      <h2>Courses offered by Skillsoft</h2>
+      <p></p>
+      <Course popCourse={mostPopular}/>
     </div>
   );
 }
---
+++ src/Components/header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header(){
+
+    return(
+        <header>This is a header</header>
+    )
+
+}
+
+export default Header;
